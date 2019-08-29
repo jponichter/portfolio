0c5cb97de42b628a91dbba03964e7ad08487eaca
jponichter <ponichterjot@gmail.com>
2019-08-30 00:50:03 +0200

    navbar update and RWD in progress

--- app.js
+++ app.js
@@ -2,40 +2,37 @@ const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
   const navLinks = document.querySelectorAll(".nav-links li");
+  
 
-  burger.addEventListener("click", () => {
-    //Toggle Nav
-    nav.classList.toggle("nav-active");
-
-    //Animate Links
+  function animateLinks() {
+    const screen = window.matchMedia("(max-width: 768px)");
     navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +
-          0.7}s`;
-      }
-      
+        if (link.style.animation && screen.matches) {
+          link.style.animation = "";
+        } else if(screen.matches){
+          link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +
+            0.7}s`;
+        }
     });
+  }
 
-
+  //Toggle Nav
+  burger.addEventListener("click", () => {
+      nav.classList.toggle("nav-active");
+      animateLinks();
   });
 
   //Toggle while clicking on links
-  navLinks.forEach((link, index) => {
-    link.addEventListener("click", () => {
-        nav.classList.toggle("nav-active");
-        navLinks.forEach((link, index) => {
-          if (link.style.animation) {
-              link.style.animation = "";
-            } else {
-              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +
-                0.7}s`;
-            }
-        });
+  function linkToggle() {
+    navLinks.forEach((link, index) => {
+      link.addEventListener("click", () => {
+          nav.classList.toggle("nav-active");
+
+          animateLinks();        
       });
-  });
+    });
+  }
+  linkToggle();
 };
 
 navSlide();
-
